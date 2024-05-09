8ed861baabb6e3062f07c2ac271ca70d7f241e29
Abdallah hijazi <hijaziboudi0@gmail.com>
2024-05-09 23:49:38 +0300

    imported the sky and made it visibile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
-import Island from "../models/Island";
+import Island from '../models/Island'
+import Sky from '../models/Sky'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -33,6 +34,7 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
           
+          <Sky />
           <Island 
             position={islandPosition}
             scale={islandScale}
